a8d7195ed59d458e09237d56fa80838d40f93c34
Denis Frolov <d.fr8938@gmail.com>
2020-08-21 18:17:00 +0300

    fix arrays

--- Basic/arrays/index.php
+++ Basic/arrays/index.php
@@ -23,3 +23,34 @@ echo "</pre>";
 echo "<pre>";
 var_dump($array4);
 echo "</pre>";
+
+$colors = ["black", "white", "red", "blue", "orange"];
+$colors[] = "green";
+
+$len = count($colors);
+
+$i = 0;
+while ($i < $len){
+    if ($colors[$i] == "red")
+        unset($colors[$i]);
+    elseif ($colors[$i] == "white")
+        $colors[$i] = "green";
+    $i++;
+}
+
+echo "<pre>";
+print_r($colors);
+echo "</pre>";
+
+$peoples = [
+    ["firstname" => "James", "lastname" => "Bond", "age" => 35],
+    ["firstname" => "John", "lastname" => "Doe", "age" => 25]
+];
+
+$len = count($peoples);
+
+for ($i = 0; $i < $len; $i++) {
+    if ($peoples[$i]["firstname"])
+        echo "<p>Имя №" . ($i + 1) . ": " . $peoples[$i]["firstname"] . "</p>";
+}
+
