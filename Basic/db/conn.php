13e3678c0b7b5fa4617ee087969cad3fd7bcd35a
Denis Frolov <d.fr8938@gmail.com>
2020-08-23 00:34:06 +0300

    add in db DELETE

--- Basic/db/conn.php
+++ Basic/db/conn.php
@@ -11,3 +11,14 @@
     if (!$conn) {
         echo "<p>No connection</p>" . mysqli_connect_error();
     } //echo "<p>Connection</p>";
+
+    // Добавить
+
+    //$query = "INSERT INTO news_category VALUES (NULL, 'category3', 'description3', '1', '1')";
+    //$into = mysqli_query($conn, $query);
+
+    function del_el($id, $table, $conn) {
+        mysqli_query($conn, "DELETE FROM $table" . " WHERE id=" . "$id");
+    }
+
+    //del_el(3, "news_category", $conn);
