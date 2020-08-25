f4025971fda16f56cf03e0a01a5cdab336ebbd56
Denis Frolov <d.fr8938@gmail.com>
2020-08-25 08:13:47 -0400

    fix db

--- Basic/db/conn.php
+++ Basic/db/conn.php
@@ -21,8 +21,29 @@
     //$query = "INSERT INTO news_category VALUES (NULL, 'category3', 'description3', '1', '1')";
     //$into = mysqli_query($connect, $query);
 
+    // Удалить
     function f_delete($id, $table, $connect) {
         mysqli_query($connect, "DELETE FROM $table" . " WHERE id=" . "$id");
     }
 
-    f_delete(3, "news_category", $connect);
+    //f_delete(3, "news_category", $connect);
+
+    // Обновить
+
+//    $tab = "news";
+//    $query = "UPDATE $tab SET status=0 WHERE 'id'=1";
+//    $query = "UPDATE $tab SET h1='Hello world!!!' WHERE 'id'=2";
+//    $info = mysqli_query($connect, $query);
+//    var_dump($info);
+//
+//    $count = mysqli_affected_rows($connect);
+//    echo $count;
+
+    // Выборка
+
+    $tab = "news";
+    $query = "SELECT 'id', 'content' FROM $tab";
+    $info = mysqli_query($connect, $query);
+    echo '<pre>';
+    var_dump($info);
+    echo '</pre>';
