630f1fc125682cfba47bc1183cd1328c4c1264ac
Denis Frolov <d.fr8938@gmail.com>
2020-08-26 05:29:26 -0400

    add uniqueInOrder

--- OOP/extends/bicycle.php
+++ OOP/extends/bicycle.php
@@ -11,6 +11,16 @@ class Bicycle extends Vehicle {
         $this->speed = $speed;
     }
 
+    // Расход энергии (ккал) = 0,014 * М * t * (0,12 * П - 7)
+    // М - вес тела человека
+    // t - время заезда
+    // П - средний пульс во время заезда
+    public function caloriesBurned($weight, $pulse, $distance)
+    {
+        $time = $this->tripTime($distance) * 60; // минуты
+        return 0.014 * $weight * $time * (0.12 * $pulse - 7);
+    }
+
     public function getWheels()
     {
         return self::WHEELS;
--- OOP/extends/index.php
+++ OOP/extends/index.php
@@ -15,5 +15,34 @@ echo "Ferrari has " . $ferrari->getWheels() . " wheels<br>";
 
 echo '<hr>';
 $stern = new Bicycle("Stern", "white", 100);
-echo "time Stern = " . round($ferrari->tripTime(300), 2) . '<br>';
+echo "time Stern = " . round($stern->tripTime(300), 2) . '<br>';
+echo "calories = " . $stern->caloriesBurned(65, 100, 300) . '<br>';
 echo "Stern has " . $stern->getWheels() . " wheels";
+
+function rep($src) {
+
+    if (gettype($src) == string) {
+        $src = str_split($src);
+    }
+
+    $j = 0;
+    $n = 0;
+    $len = count($src);
+
+    $dest = array();
+    for ($i = 0; $i < $len; $i++) {
+        if ($src[$i] == $src[$i + 1]) {
+            unset($src[$i]);
+            $j++;
+        }
+        else {
+            $dest[$n] = $src[$i];
+            $n++;
+        }
+    }
+    return $dest;
+}
+
+$src = [1,2,2,3,3];
+echo gettype($src);
+print_r(rep($src));
