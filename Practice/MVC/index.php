ae8c854d4b6682fa652d6abab1ec8cf73cd62a53
Denis Frolov <d.fr8938@gmail.com>
2020-08-29 08:37:56 -0400

    add folder tmp

--- Practice/MVC/index.php
+++ Practice/MVC/index.php
@@ -1,20 +1,5 @@
 <?php
 
-//$string = '08:00 22-01-1989';
-//echo 'Исходная строка: ' . $string . '<br>';
-//
-//$pattern = '/([0-9]{2}):([0-9]{2}) ([0-9]{2})-([0-9]{2})-([0-9]{4})/';
-//
-//$newString = preg_match($pattern, $string);
-//if ($newString) echo 'yes';
-//else echo 'no';
-//
-//$replacenment = '$1:$2 $4-$3-$5';
-//echo '<hr>';
-//echo preg_replace($pattern, $replacenment, $string);
-//
-//echo '<hr>';
-
 echo "<h1>Index page</h1><hr>";
 
 /*
